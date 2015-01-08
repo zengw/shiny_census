860bf1b87e03929474ec0af30df6374a45b11e7d
zengw <zengwei917@gmail.com>
2015-01-07 19:23:16 -0800

    my first commit

---
+++ ui.R
@@ -0,0 +1,51 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("My Shiny App"),
+  
+  # Sidebar with a slider input 
+  sidebarLayout(
+    sidebarPanel(h3("Installation"),
+                 p("Shiny is available on CRAN, so you can install it in the usual way from your R console:"),
+                 code('install.packages("shiny")'),
+                 br(),
+                 br(),
+                 br(),              
+                 img(src="bigorb.png",height=50,width=50),
+                 "Shiny is a product of", 
+                   span("Rstudio", style="color:blue"),
+                 helpText("Create demographic maps with information from the 2010 US census. "),
+                 fluidRow(
+                   column(12,
+                          selectInput("select", 
+                                      label=strong("Select a Variable to display"),
+                                     choices=list("Percent White", "Percent Black"),
+                                         selected="Percent White"))),
+                 fluidRow(
+                   column(12,
+                          sliderInput("slider1",label=strong("Range of Interest"),
+                                      min=0, max=100,value=c(0,100))))
+                 
+                ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(h2("Intoducing Shiny"),
+              p("Shiny is a new package from RStudio that makes it",
+                em("incredibly easy"),
+                "to build interactive web application with R."),
+              br(),
+              p("For an introdution and live examples, visit then", 
+                a("Shiny homepage.", 
+                   href="http://www.google.com", target="_blank")),
+              h3("Features"),
+              p("* Build useful webpages"),
+              p("*Shiny application",
+                strong("spreadsheets"))
+  
+              )
+  )
+
+))
